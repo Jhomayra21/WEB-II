0988542a8b1d6ce9bdeb359385934ba50c9c9675
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-02-24 08:27:52 -0400

    java

---
+++ uso_js/02_nombres_variables.js
@@ -0,0 +1,4 @@
+var nombre="Jhomayra";
+let direccion="6 de agosto";
+const hola="holis";
+let edad=4;
---
+++ uso_js/05_llamado_consola.js
@@ -0,0 +1,11 @@
+const valor=2121;
+console.log(valor);
+
+const apellido ="Mamani";
+const nombre ="Jhomayra";
+
+console.log(apellido);
+console.log(nombre);
+
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
