dbcb18cc20cda0f4f763347179db09d0a16220c4
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-03-17 08:37:39 -0400

    dom

---
+++ dom/componentes/checkComplete.js
@@ -0,0 +1,18 @@
+const checkComplete=()=>{
+    const i = document.createElement('i');                          //creacion de un icono
+    i.classList.add("far","fa-check-square","icon");           //dar estilo al icono
+    i.addEventListener("click",color);                              //llamar a la funcion color
+    return i;
+}
+
+const color =(evento)=>{
+    const element = evento.target;                                   //target devuelve informacion
+    element.classList.add('fas');                                    //es un estilo de esto "far","fa-check-square icon","icon"
+    element.classList.remove('completeIcon');                           // aqui era add
+    element.classList.remove('far');
+}
+
+
+
+export default checkComplete;                                          //para que exporte y se pueda llamar desde el otro java 
+
