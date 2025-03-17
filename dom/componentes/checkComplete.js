61bf998067c2dc817f0f2d1f0b4365a032e1f012
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-03-17 14:11:37 -0400

    checkComplete.js

--- dom/componentes/checkComplete.js
+++ dom/componentes/checkComplete.js
@@ -5,14 +5,24 @@ const checkComplete=()=>{
     return i;
 }
 
+// const color =(evento)=>{
+//     const element = evento.target;                                   //target devuelve informacion
+//     element.classList.add('fas');                                    //es un estilo de esto "far","fa-check-square icon","icon"
+//     element.classList.remove('completeIcon');                           // aqui era add
+//     element.classList.remove('far');
+// }
 const color =(evento)=>{
-    const element = evento.target;                                   //target devuelve informacion
-    element.classList.add('fas');                                    //es un estilo de esto "far","fa-check-square icon","icon"
-    element.classList.remove('completeIcon');                           // aqui era add
-    element.classList.remove('far');
+    const element = evento.target; 
+    if (element.classList.contains('fas')) {
+        element.classList.remove('fas');
+        element.classList.add('far');
+    } else {
+        element.classList.add('fas');
+        element.classList.remove('far');
+    }
 }
 
 
-
 export default checkComplete;                                          //para que exporte y se pueda llamar desde el otro java 
+
 
