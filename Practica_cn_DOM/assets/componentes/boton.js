6ed29384ffd8b00968e558319cd8165a616996a0
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-03-24 10:08:28 -0400

    Practica_cn_DOM

---
+++ Practica_cn_DOM/assets/componentes/boton.js
@@ -0,0 +1,22 @@
+const boton = document.getElementById('btnModo')
+    const link = document.getElementById('head__link');
+    const nav = document.getElementById('head__nav');
+    const elemento = document.getElementById('elemento');  
+    const mensaje = document.getElementById('mensaje__item');
+    const nosotros = document.getElementById('nosotros');
+    const producto = document.getElementById('card');
+    const contacto = document.getElementById('contacto');
+    const footer = document.getElementById('footer ');
+   
+ 
+    //elemento.style.backgroundColor('#ffffff');
+ 
+    boton.addEventListener('click', () =>{
+        if (elemento.style.backgroundColor===('#ffffff')){
+            elemento.style.backgroundColor='black';
+            boton.textContent('modo claro')
+        }
+        else{
+            elemento.style.backgroundColor='#ffffff'
+        }
+    })
